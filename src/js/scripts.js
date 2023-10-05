1304258a4fd9232306f1b62586ce418a16c12e34
Brandon <brandonaguerodeveloper@gmail.com>
2023-10-05 15:45:07 -0500

    Se agregaron las validaciones para enviarme un mensaje por el formulario correctamente. Además se agregaron nuevas etiquetas html y estilos extras

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,16 +2,24 @@ const inputName = document.querySelector(".form__input--name");
 const inputEmail = document.querySelector(".form__input--email");
 const inputMessage = document.querySelector(".form__textarea");
 const inputSubmit = document.querySelector(".div__form");
+const messagesError = document.querySelectorAll(".div__p--error");
+const botTrap = document.querySelector("#bot_trap");
 
 inputName.addEventListener("input", (event) => {
   const valueLength = event.currentTarget.value.length;
 
   if (valueLength <= 2 && valueLength >= 1) {
     inputName.style.borderBottomColor = "#ff6f5b";
+    messagesError[0].style.display = "block";
+    messagesError[0].textContent = "Input too short, please try again.";
   } else if (valueLength >= 3) {
     inputName.style.borderBottomColor = "#4ee1a0";
+    messagesError[0].textContent = "";
+    messagesError[0].style.display = "none";
   } else {
     inputName.style.borderBottomColor = "#ffffff";
+    messagesError[0].textContent = "";
+    messagesError[0].style.display = "none";
   }
 });
 
@@ -34,9 +42,15 @@ inputEmail.addEventListener("input", (event) => {
 
   if (validationsError) {
     inputEmail.style.borderBottomColor = "#ff6f5b";
+    messagesError[1].style.display = "block";
+    messagesError[1].textContent = "Sorry, invalid format here";
   } else if (validationsThen) {
     inputEmail.style.borderBottomColor = "#4ee1a0";
+    messagesError[1].textContent = "";
+    messagesError[1].style.display = "none";
   } else {
+    messagesError[1].textContent = "";
+    messagesError[1].style.display = "none";
     inputEmail.style.borderBottomColor = "#ffffff";
   }
 });
@@ -46,37 +60,73 @@ inputMessage.addEventListener("input", (event) => {
 
   if (valueLength <= 19 && valueLength >= 1) {
     inputMessage.style.borderBottomColor = "#ff6f5b";
+    messagesError[2].style.display = "block";
+    messagesError[2].textContent = "Input too short, please try again.";
   } else if (valueLength >= 20) {
     inputMessage.style.borderBottomColor = "#4ee1a0";
+    messagesError[2].textContent = "";
+    messagesError[2].style.display = "none";
   } else {
     inputMessage.style.borderBottomColor = "#ffffff";
+    messagesError[2].textContent = "";
+    messagesError[2].style.display = "none";
   }
 });
 
 function main() {
   inputSubmit.addEventListener("submit", (e) => {
+    if (botTrap.value.length >= 1) {
+      e.preventDefault();
+      return;
+    }
+
     if (
       inputName.value.length <= 2 ||
       inputEmail.value.length <= 10 ||
       inputMessage.value.length <= 20
     ) {
       e.preventDefault();
+      if (inputName.value.length === 0) {
+        inputName.style.borderBottomColor = "#ff6f5b";
+        messagesError[0].style.display = "block";
+        messagesError[0].textContent = "Input empty, please try again";
+      }
+      if (inputEmail.value.length === 0) {
+        inputEmail.style.borderBottomColor = "#ff6f5b";
+        messagesError[1].style.display = "block";
+        messagesError[1].textContent = "Input empty, please try again";
+      }
+
+      if (inputMessage.value.length === 0) {
+        inputMessage.style.borderBottomColor = "#ff6f5b";
+        messagesError[2].style.display = "block";
+        messagesError[2].textContent = "Input empty, please try again";
+      }
       return;
     }
 
     if (inputEmail.value.includes(" ")) {
       e.preventDefault();
+      inputEmail.style.borderBottomColor = "#ff6f5b";
+      messagesError[1].style.display = "block";
+      messagesError[1].textContent = "Sorry, invalid format here";
       return;
     }
 
     if (!inputEmail.value.endsWith("@gmail.com")) {
       e.preventDefault();
+      inputEmail.style.borderBottomColor = "#ff6f5b";
+      messagesError[1].style.display = "block";
+      messagesError[1].textContent = "Sorry, invalid format here";
       return;
     }
 
     const nameUserEmail = inputEmail.value.split("").slice(0, -10);
     if (nameUserEmail.includes("@")) {
       e.preventDefault();
+      inputEmail.style.borderBottomColor = "#ff6f5b";
+      messagesError[1].style.display = "block";
+      messagesError[1].textContent = "Sorry, invalid format here";
       return;
     }
   });
